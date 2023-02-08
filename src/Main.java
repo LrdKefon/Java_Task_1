c84f07e42cbf3c085610da098f3621491f9603ac
Alex <zazerny@gmail.com>
2023-02-08 17:51:30 +0300

    Task resolved

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int cost = 15270; //стоимость билета
+        int x = 20;       // 1 миля за каждые 20 рублей
+        int bonus = cost / x;
+
+        System.out.println("Your accrued bonus miles:" + bonus);
     }
+
 }
